96eb1420d30847ac6a205899e10bf21c17546ff2
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-04-27 21:31:10 +0600

    user create , delete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import Adduser from './Components/AddUser/Adduser';
 import Home from './Components/Home/Home';
+import User from './Components/Users/User';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/user' element={<User></User>}></Route>
         <Route path='user/add' element={<Adduser></Adduser>}></Route>
       </Routes>
     </div>
---
+++ src/Components/Users/User.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+
+const User = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, []);
+
+    const handleDeleteUser = id => {
+        const proceed = window.confirm('Are you sure to delete ? ');
+        if (proceed) {
+            console.log('delete', id);
+            const url = `http://localhost:5000/user/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        console.log('deleted successful');
+                        const remaing = users.filter(user => user._id !== id);
+                        setUsers(remaing)
+                    }
+                })
+        }
+    }
+    return (
+        <div>
+            <h2>Avaiable users {users.length}</h2>
+            <ul >
+                {
+                    users.map(user => <li className='p-3' key={user._id}> {user.name} === {user.email}
+                        <button onClick={() => handleDeleteUser(user._id)} className='m-2 btn-danger'>x</button></li>)
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default User;
